4e3feac8fca4c92b8b78f02767f08423fda6244f
sorachung <sorachung.src@gmail.com>
2022-03-21 11:09:45 -0500

    added JournalManager with List functionality and implemented it in MainMenuManager

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("My Journal Menu");
+            Console.WriteLine(" 1) List Journals");
+            Console.WriteLine(" 2) Add Journal");
+            Console.WriteLine(" 3) Edit Journal");
+            Console.WriteLine(" 4) Remove Journal");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    return this;
+                case "3":
+                    return this;
+                case "4":
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach(Journal journal in journals)
+            {
+                Console.WriteLine(journal);
+            }
+        }
+    }
+}
