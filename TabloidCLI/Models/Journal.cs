8ea7e59b94db1b1ed118ec06c7faa43049a3daa3
sorachung <sorachung.src@gmail.com>
2022-03-21 10:28:07 -0500

    added Journal model for real

--- TabloidCLI/Models/Journal.cs
+++ TabloidCLI/Models/Journal.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace TabloidCLI.Models
 {
-    internal class Journal
+    public class Journal
     {
+        public string Title {get; set;}
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} created on {CreateDateTime} : {Content}";
+        }
     }
 }
