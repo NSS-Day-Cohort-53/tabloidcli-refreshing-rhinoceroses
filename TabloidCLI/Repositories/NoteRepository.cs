10a4f79bfae117597c87a578d8b573bc4871444b
sorachung <sorachung.src@gmail.com>
2022-03-22 10:50:41 -0500

    made barebones note repo and note manager

---
+++ TabloidCLI/Repositories/NoteRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    public class NoteRepository : DatabaseConnector, IRepository<Note>
+    {
+        public NoteRepository(string connectionString) : base (connectionString) { }
+
+        public List<Note> GetAll()
+        {
+            return null;
+        }
+
+        public Note Get(int id)
+        {
+            return null;
+        }
+
+        public void Insert(Note note)
+        {
+
+        }
+
+        public void Update(Note note)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
